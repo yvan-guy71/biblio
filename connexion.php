fbe846f82885bad52f44d64317419be66dc03ead
guyzo999 <armellesage66@gmail.com>
2025-12-18 16:21:11 +0000

    connexion

--- connexion.php
+++ connexion.php
@@ -4,8 +4,11 @@ $user = getenv('DB_USER') ?: 'root';
 $password = getenv('DB_PASSWORD') ?: '';
 $db = getenv('DB_NAME') ?: 'book_store';
 
+// Debug: Afficher les valeurs (supprimez après test)
+echo "Host: $host, User: $user, DB: $db<br>";
+
 $con = mysqli_init();
-$con->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);  // Désactiver la vérification SSL pour PlanetScale
+$con->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
 $con->real_connect($host, $user, $password, $db);
 
 if ($con->connect_error) {
